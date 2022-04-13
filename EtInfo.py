917bb80c933a4b357cd760cead4e3aa9497a186e
Mostafa-ma2 <mostafaabedinima2@gmail.com>
2022-04-13 20:41:38 +0430

    add whois to main and exit code on EtInfo

--- EtInfo.py
+++ EtInfo.py
@@ -1,18 +1,48 @@
 #!/usr/bin/env python
 
 import sys
-import os 
+import os
 import atexit
+from tabnanny import check
+import time
+
+from sqlalchemy import false
+
+
 from lib.helper import helper
+from lib.colors import colors
 from lib.banner import banner
+from modules.whois import whois
+
 # ----------  Variables  ---------------
-is_running= True # project has running
-app_version="1.0.0"
+is_running = True  # project has running
+app_version = "1.0.0" # app version
+data = {}
+
+def main():
+    global is_running
+    option = input(colors.Green+'[' + format(time.strftime("%H:%M:%S", time.localtime()
+                                                           )) + ']' + colors.White+' Please select an option ' + colors.Cyan+': ')
+    if option == "1" or option.lower() == "whois":
+        ip_address = input(
+            colors.Green + '[ + ]' + colors.White + 'Please type (ip/website) '+colors.Cyan+': ')
+        whois(helper(None,ip_address).check_ipAdress(),data).whois_lookup()
+    
+    
+    if option == "0" or option.lower() == "exit":
+        do_want=False
+    else:
+        do_want=input('\n'+colors.Green+'[' + format(time.strftime("%H:%M:%S", time.localtime()
+                                                           )) + ']' + colors.White+' Do you want to continue?(y/n) ' + colors.Cyan+': ')
+    if do_want.lower() == "n" or do_want.lower() == "no":
+        print('\n'+colors.Green + '[ + ]' + colors.White + 'Good luck '+colors.Cyan+'! ')
+        is_running  = False
+
 
 if __name__ == "__main__":
     helper().pkg_install()
     helper().check_internet()
-    if is_running:
+    while is_running:
         helper().clear()
         banner.banner()
-        
+        main()
