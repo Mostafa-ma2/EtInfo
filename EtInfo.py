25c7c211fbdbfdfaf21d272479763609bdbac019
Mostafa-ma2 <mostafaabedinima2@gmail.com>
2022-04-16 17:34:34 +0430

    code cleen and add dns record on banner and EtInfo

--- EtInfo.py
+++ EtInfo.py
@@ -8,33 +8,37 @@ import importlib.util
 from lib.colors import colors
 # ----------  Variables  ---------------
 is_running = True  # project has running
-app_version = "1.0.0" # app version
+app_version = "1.0.0"  # app version
 data = {}
-project_path = os.path.dirname(os.path.realpath(__file__)) 
+project_path = os.path.dirname(os.path.realpath(__file__))
+
 
 def main():
     global is_running
     option = input(colors.Green+'[' + format(time.strftime("%H:%M:%S", time.localtime()
                                                            )) + ']' + colors.White+' Please select an option ' + colors.Cyan+': ')
+    ip_address = input('\n' +
+                       colors.Green + '[ + ] ' + colors.White + 'Please type (ip/website) '+colors.Cyan+': ')
+    
     if option == "1" or option.lower() == "whois":
-        ip_address = input('\n'+
-            colors.Green + '[ + ] ' + colors.White + 'Please type (ip/website) '+colors.Cyan+': ')
-        whois(helper(None,ip_address).check_ipAdress(),data).whois_lookup()
-    
+        whois(helper(None, ip_address).check_ipAdress(), data).whois_lookup()
+
     elif option == "2" or option.lower() == "port scan":
-        ip_address = input('\n'+
-            colors.Green + '[ + ] ' + colors.White + 'Please type (ip/website) '+colors.Cyan+': ')
-        port_scan(ipAddress=helper(None,ip_address).check_ipAdress()).scan()
-    
-    
+        port_scan(ipAddress=helper(None, ip_address).check_ipAdress()).scan()
+
+    elif option == "3" or option.lower() == "dns record":
+        dns_record(ipAddress=helper(None, ip_address).check_ipAdress()).dns_record()
+
     if option == "0" or option.lower() == "exit":
-        do_want='no'
+        do_want = 'no'
     else:
-        do_want=input('\n'+colors.Green+'[' + format(time.strftime("%H:%M:%S", time.localtime()
-                                                           )) + ']' + colors.White+' Do you want to continue?(y/n) ' + colors.Cyan+': ')
+        do_want = input('\n'+colors.Green+'[' + format(time.strftime("%H:%M:%S", time.localtime()
+                                                                     )) + ']' + colors.White+' Do you want to continue?(y/n) ' + colors.Cyan+': ')
     if do_want.lower() == "n" or do_want.lower() == "no":
-        print('\n'+colors.Green + '[ + ]' + colors.White + ' Good luck '+colors.Cyan+'! ')
-        is_running  = False
+        print('\n'+colors.Green + '[ + ]' +
+              colors.White + ' Good luck '+colors.Cyan+'! ')
+        is_running = False
+
 
 def pkg_install(path):
     fail = True
@@ -50,19 +54,21 @@ def pkg_install(path):
             pass
     if fail == False:
         print('\n' + colors.Red + '[-]' + colors.Cyan + ' Please Execute ' + colors.White +
-                'pip3 install -r requirements.txt' + colors.Cyan + ' to Install Missing Packages' + colors.White + '\n')
+              'pip3 install -r requirements.txt' + colors.Cyan + ' to Install Missing Packages' + colors.White + '\n')
         sys.exit()
-    
+
+
 if __name__ == "__main__":
     pkg_install(project_path)
-    
+
     # --------- import file ----------------
     from lib.helper import helper
     from lib.banner import banner
     from modules.whois import whois
     from modules.port_scan import port_scan
-    helper().check_internet() #check internet connection
+    from modules.dns_record import dns_record
+    helper().check_internet()  # check internet connection
     while is_running:
-        helper().clear() # cleen terminal
-        banner.banner() 
+        helper().clear()  # cleen terminal
+        banner.banner()
         main()
